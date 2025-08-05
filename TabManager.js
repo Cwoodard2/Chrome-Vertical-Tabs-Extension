5d4ac27a871b1e3863b69ac998a4a9aa7bb62187
Cwoodard2 <ctwoodard2023@gmail.com>
2025-08-04 22:57:41 -0500

    - Added functionality to remove a tab - Added functionality to select a tab - Tabs change position accordingly if moved via the horizontal bar

--- TabManager.js
+++ TabManager.js
@@ -1,12 +1,39 @@
-const tabs = await chrome.tabs.query({});
-console.log(tabs);
+export async function grabTabs() {
+  const tabs = await chrome.tabs.query({});
+  console.log(tabs);
 
-const tabList = tabs.map(currentTab => {
-    return `<li>${currentTab.title}</li>`;
-}).join('');
+  const tabList = tabs
+    .map((currentTab) => {
+      console.log("active");
+      return `<li style=${currentTab.active && "background-color:grey;"} id=${currentTab.id} class="tab">
+                <p>${currentTab.title}</p> 
+                <button id=${currentTab.id + "button"} >X</button>
+            </li>`;
+    })
+    .join("");
 
-console.log(tabList);
-// tabList.forEach(element => {
-//     document.querySelector("ul").append(element);
-// });
-document.getElementById("tabList").innerHTML = tabList;
+
+  // console.log(tabList);
+
+  document.getElementById("tabList").innerHTML = tabList;
+
+  tabs.forEach(element => {
+    document.getElementById(element.id).addEventListener("click", () => {
+        chrome.tabs.update(element.id, {"active": true});
+    })
+
+    document.getElementById(element.id + "button").addEventListener("click", () => {
+      chrome.tabs.remove(element.id, () => {
+        console.log("tab removed");
+      });
+    })
+  });
+}
+
+grabTabs();
+
+chrome.tabs.onUpdated.addListener(() => {
+  console.log("noticed update");
+});
+chrome.tabs.onRemoved.addListener(grabTabs);
+chrome.tabs.onMoved.addListener(grabTabs);
