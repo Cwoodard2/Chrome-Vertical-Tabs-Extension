b3e20aeb78cde402414848410f14dca1c22cb657
Cwoodard2 <cawooda@siue.edu>
2025-08-10 21:48:09 -0500

    - Added ability to view tab group colors - Added "add tab" button - Removed "up" and "down" buttons for the moment - Tabs now correctly update when clicked on to show they are active

--- TabManager.js
+++ TabManager.js
@@ -4,57 +4,83 @@ export async function grabTabs() {
 
   const tabList = tabs
     .map((currentTab) => {
-      return `<li draggable="true" style=${currentTab.active && "background-color:grey;"} id=${currentTab.id} class="tab">
-                <img src=${currentTab.favIconUrl} style="width:24px;height:24px;"/>
-                <p>${currentTab.title}</p> 
-                <button id=${currentTab.id + "up-button"}>up</button>
-                <button id=${currentTab.id + "down-button"}>Down</button>
+      return `<li draggable="true" style=${
+        currentTab.active && "background-color:grey;"
+      } id=${currentTab.id} class="tab">
+      <div class="tab">
+                <img src=${
+                  currentTab.favIconUrl
+                } style="width:24px;height:24px;"/>
+                <p>${currentTab.title}</p>
+                </div>
+                <div>
                 <button id=${currentTab.id + "close-button"} >X</button>
+                </div> 
             </li>`;
     })
     .join("");
 
+  // <button id=${currentTab.id + "up-button"}>up</button>
+  //             <button id=${currentTab.id + "down-button"}>Down</button>
 
   document.getElementById("tabList").innerHTML = tabList;
 
-  tabs.forEach(element => {
-    // getTabGroup(element.groupId);
+  tabs.forEach(async (element) => {
     const currentElement = document.getElementById(element.id);
+    console.log(element.groupId);
+    if (element.groupId != -1) {
+      const groupColor = await getTabGroup(element.groupId);
+      currentElement.classList.add(groupColor);
+    }
     currentElement.addEventListener("click", () => {
-        chrome.tabs.update(element.id, {"active": true}, () => {
-          console.log("Completed Update");
-        });
-    })
+      chrome.tabs.update(element.id, { active: true }, () => {
+        console.log("Hey we updated!");
+        grabTabs();
+      });
+    });
 
     //add handling for if tab is first in list
-    document.getElementById(element.id + "up-button").addEventListener("click", () => {
-        chrome.tabs.move(element.id, {"index": element.index-1});
-    })
-    
-    //Add handling for if tab is last in list
-    document.getElementById(element.id + "down-button").addEventListener("click", () => {
-        chrome.tabs.move(element.id, {"index": element.index+1});
-    })
+    // document
+    //   .getElementById(element.id + "up-button")
+    //   .addEventListener("click", () => {
+    //     chrome.tabs.move(element.id, { index: element.index - 1 });
+    //   });
 
-    document.getElementById(element.id + "close-button").addEventListener("click", () => {
-      chrome.tabs.remove(element.id, () => {
-        console.log("tab removed");
+    // //Add handling for if tab is last in list
+    // document
+    //   .getElementById(element.id + "down-button")
+    //   .addEventListener("click", () => {
+    //     chrome.tabs.move(element.id, { index: element.index + 1 });
+    //   });
+
+    document
+      .getElementById(element.id + "close-button")
+      .addEventListener("click", () => {
+        chrome.tabs.remove(element.id, () => {
+          console.log("tab removed");
+        });
       });
-    })
   });
 }
 
-// function getTabGroup() {
-//   chrome.tabGroups.query({});
-//   console.log("group");
-// }
+document.getElementById("add-tab-button").addEventListener("click", () => {
+  chrome.tabs.create({});
+})
+
+async function getTabGroup(elementId) {
+  const tabGroup = await chrome.tabGroups.get(elementId);
+  console.log(tabGroup);
+  return tabGroup.color;
+}
 
 grabTabs();
 // getTabGroup();
 
-// chrome.tabs.onUpdated.addListener((tab) => {
-//   console.log("noticed update");
-//   console.log(tab);
-// });
+chrome.tabs.onUpdated.addListener((tab) => {
+  console.log("noticed update");
+  console.log(tab);
+  grabTabs();
+});
+chrome.tabs.onCreated.addListener(grabTabs);
 chrome.tabs.onRemoved.addListener(grabTabs);
 chrome.tabs.onMoved.addListener(grabTabs);
