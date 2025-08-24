a36bcff27fff3e264af66f27c75f1a174bb5e9d5
Cwoodard2 <cawooda@siue.edu>
2025-08-24 14:43:49 -0500

    - Refactored Tab Manager to be multiple functions - Added rudimentary skeleton for tab group support - Implemented support for dark and light mode

--- TabManager.js
+++ TabManager.js
@@ -1,13 +1,47 @@
-export async function grabTabs() {
-  const tabs = await chrome.tabs.query({});
-  console.log(tabs);
+async function getTabGroups() {
+  const tabGroups = await chrome.tabGroups.query({});
+  console.log(tabGroups);
+  let tabsInGroups = [];
+  let finalGroups;
+  tabGroups.forEach(async (element, index) => {
+    const title = element.title.replaceAll(' ', '');
+    const group = await chrome.tabs.query({ groupId: element.id });
+    console.log(group);
+    let groupName = title + "-group";
+    console.log(groupName);
+    finalGroups = `<li class="tab-group" id=${
+      groupName
+    }>${title}<ul id=${
+      title
+    } class="">${buildTabs(group)}</ul></li>`;
 
+    document
+      .getElementById("tab-group-list")
+      .insertAdjacentHTML("beforeend", finalGroups);
+
+    addGroupListeners(group, title);
+  });
+}
+
+function addGroupListeners(group, element) {
+  document
+    .getElementById(element + "-group")
+    .addEventListener("click", () => {
+      const tabGroup = document.getElementById(element);
+      console.log(tabGroup.classList.contains("hide-group"));
+      tabGroup.classList.contains("hide-group") ? tabGroup.classList.remove("hide-group") : tabGroup.classList.add("hide-group");
+    });
+
+  addListeners(group);
+}
+
+function buildTabs(tabs) {
   const tabList = tabs
     .map((currentTab) => {
       return `<li draggable="true" style=${
         currentTab.active && "background-color:grey;"
       } id=${currentTab.id} class="tab">
-      <div class="tab">
+      <div class="tab-content">
                 <img src=${
                   currentTab.favIconUrl
                 } style="width:24px;height:24px;"/>
@@ -19,12 +53,10 @@ export async function grabTabs() {
             </li>`;
     })
     .join("");
+  return tabList;
+}
 
-  // <button id=${currentTab.id + "up-button"}>up</button>
-  //             <button id=${currentTab.id + "down-button"}>Down</button>
-
-  document.getElementById("tabList").innerHTML = tabList;
-
+async function addListeners(tabs) {
   tabs.forEach(async (element) => {
     const currentElement = document.getElementById(element.id);
     console.log(element.groupId);
@@ -37,6 +69,10 @@ export async function grabTabs() {
         console.log("Hey we updated!");
         grabTabs();
       });
+    });
+
+    currentElement.addEventListener("dragstart", () => {
+      console.log("starting drag");
     });
 
     //add handling for if tab is first in list
@@ -63,9 +99,26 @@ export async function grabTabs() {
   });
 }
 
+export async function grabTabs() {
+  //change to get all tab groups first
+  getTabGroups();
+  const tabs = await chrome.tabs.query({
+    groupId: chrome.tabGroups.TAB_GROUP_ID_NONE,
+  });
+  console.log(tabs);
+  // get all tabs in tab groups
+  //then get all tabs that aren't in a group
+  const tabList = buildTabs(tabs);
+
+  document.getElementById("tab-list").insertAdjacentHTML("beforeend", tabList);
+
+  addListeners(tabs);
+  // addListeners();
+}
+
 document.getElementById("add-tab-button").addEventListener("click", () => {
-  chrome.tabs.create({});
-})
+  chrome.tabs.create({ index: 0 });
+});
 
 async function getTabGroup(elementId) {
   const tabGroup = await chrome.tabGroups.get(elementId);
@@ -74,6 +127,7 @@ async function getTabGroup(elementId) {
 }
 
 grabTabs();
+getTabGroups();
 // getTabGroup();
 
 chrome.tabs.onUpdated.addListener((tab) => {
